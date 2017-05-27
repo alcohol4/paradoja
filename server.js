ba41fc1ecb0f7559796427b4e5ac28db19c7b83d
root <root@DESKTOP-MM2KMD8.localdomain>
2017-05-27 03:38:19 -0700

    update

--- server.js
+++ server.js
@@ -12,132 +12,54 @@ server.listen(4000);
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/tablero.html")
 });
-var id = 0;
-var nicknames = {};
-var jugadorBlancas = '';
-var jugadorNegras = '';
-var tablero;
-var turnBlancas = true;
-var turnNegras = false;
-var ableChangeTurn;
-var timeTurno = false;
+var jugador = {blancas: '',
+                 negras: ''};
+var lista_espera = [];
+var indice = 0;
+var turno = {blancas : true,
+              negras: false}
+var blockPiece = { status: false,
+  id:''
+};
 io.sockets.on('connection', function(socket) {
-    if (turnBlancas == true) {
-        turnBlancas = false;
-        turnNegras = true;
-    } else if (turnNegras == true) {
-        turnBlancas = true;
-        turnNegras = false;
+  socket.on('crear jugador', function(data) {
+      socket.nickJugador=data;
+      if(jugador.blancas == ''){
+        jugador.blancas = socket.nickJugador;
+      }else if(jugador.negras == ''){
+        jugador.negras=socket.nickJugador;
+      }else{
+        lista_espera[indice] = socket.nickJugador;
+        indice++;
+      }
+  });
+  socket.on('validar seleccion peon', function(data) {
+if(data.nickJugador == jugador.blancas && data.colorPiece == 'rgb(255, 255, 255)' && turno.blancas == true && blockPiece.status == false){
+  io.sockets.emit('comunicar seleccion', true);
+}
+  })
+  socket.on('validar movimiento peon', function(data) {
+    filaDif = data.targetFila - data.fichaFila;
+    if(filaDif == -1 || filaDif == 1){
+
+      if(blockPiece.status == true && data.fichaId==blockPiece.id){
+        console.log(blockPiece.status)
+blockPiece.id=data.targetId;
+        io.sockets.emit('comunicar movimiento', true);
+      }else if(blockPiece.status == false){
+
+        io.sockets.emit('comunicar movimiento', true);
+        blockPiece.id=data.targetId;
+        blockPiece.status = true;
+
+
+      }else{
+        console.log(blockPiece.status)
+        console.log(blockPiece.id)
+        console.log(data.fichaId)
+      }
     }
-    var turnos = {
-        turnBlancas,
-        turnNegras
-    }
-    io.sockets.emit('carga variables', turnos);
-
-    function cambiarTurno() {
-        if (turnBlancas == true) {
-            turnBlancas = false;
-            turnNegras = true;
-        } else if (turnNegras == true) {
-            turnBlancas = true;
-            turnNegras = false;
-        }
-        var turnos = {
-            turnBlancas,
-            turnNegras
-        }
-        io.sockets.emit('setear turno', turnos);
-        timeTurno = false;
-    }
-
-    socket.on('cambiar turno', function(data) {
-        if (timeTurno == false) {
-            setTimeout(cambiarTurno, 3000);
-            timeTurno = 'true'
-        }
-    })
-
-    if (timeTurno == false) {
-        setTimeout(cambiarTurno, 3000);
-        timeTurno = 'true'
-    }
-    socket.on('send message', function(data) {
-        io.sockets.emit('new message', {
-            msg: data,
-            nick: socket.nickname
-        });
-    });
-    socket.on('new user', function(data, callback) {
-
-        if (data in nicknames) {
-            callback(false);
-        } else {
-            callback(true);
-            socket.nickname = data;
-            nicknames[socket.nickname] = 1;
-            updateNickNames();
-        }
-    });
-    socket.on('disconnect', function(data) {
-        if (!socket.nickname) return;
-        delete nicknames[socket.nickname];
-        updateNickNames();
-    });
-    socket.on('set player blancas', function(data) {
-        jugadorBlancas = socket.nickname;
-        if (jugadorBlancas != '' && jugadorNegras != '') {
-            io.sockets.emit('empieza el juego')
-
-        }
-        if (socket.nickname) {
-            io.sockets.emit('player blancas', {
-                nick: jugadorBlancas
-            });
-        } else {
-            return 'error';
-        }
-    });
-    socket.on('set player negras', function(data) {
-        jugadorNegras = socket.nickname;
-        if (jugadorBlancas != '' && jugadorNegras != '') {
-            console.log("asdsad")
-        }
-        if (socket.nickname) {
-            io.sockets.emit('player negras', {
-                nick: jugadorNegras
-            });
-        } else {
-            return 'error';
-        }
-    });
-    socket.on('validate player blancas', function(data) {
-        if (data == socket.nickname) {
-            io.sockets.emit('check player blancas', true);
-        } else {
-            io.sockets.emit('check player blancas', false);
-        }
-    });
-    socket.on('validate player negras', function(data) {
-        if (data == socket.nickname) {
-            io.sockets.emit('check player negras', true);
-        } else {
-            io.sockets.emit('check player negras', false);
-        }
-    });
-    socket.on('update tablero', function(data) {
-        io.sockets.emit('refresh tablero', data);
-    });
-
-    function updateNickNames() {
-        io.sockets.emit('usernames', nicknames);
-        io.sockets.emit('player blancas', {
-            nick: jugadorBlancas
-        });
-        io.sockets.emit('player negras', {
-            nick: jugadorNegras
-        });
-    }
+  })
 })
 
 app.use(express.static(__dirname + "/"));
