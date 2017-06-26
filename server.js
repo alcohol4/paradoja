8137f446c6b5f931c2e3aa5f29ea342b838a07ec
alco4 <alancorcos@hotmail.com>
2017-06-26 13:16:31 -0700

    upd

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get("/", function(req, res) {
 });
 var jugador = {blancas: '',
                  negras: ''};
+var turnoJugador='';
 var lista_espera = [];
 var indice = 0;
 var turno = {blancas : true,
@@ -26,6 +27,7 @@ io.sockets.on('connection', function(socket) {
       socket.nickJugador=data;
       if(jugador.blancas == ''){
         jugador.blancas = socket.nickJugador;
+        turnoJugador = jugador.blancas;
       }else if(jugador.negras == ''){
         jugador.negras=socket.nickJugador;
       }else{
@@ -33,32 +35,97 @@ io.sockets.on('connection', function(socket) {
         indice++;
       }
   });
+  socket.on('pasar turno', function(data) {
+    blockPiece.status = false;
+    io.sockets.emit('desbloquear pieza', data);
+    if(turnoJugador == jugador.blancas){
+      turnoJugador = jugador.negras;
+    }else{
+      turnoJugador = jugador.blancas;
+    }
+  })
   socket.on('validar seleccion peon', function(data) {
-if(data.nickJugador == jugador.blancas && data.colorPiece == 'rgb(255, 255, 255)' && turno.blancas == true && blockPiece.status == false){
-  io.sockets.emit('comunicar seleccion', true);
-}
+    console.log(socket.nickJugador)
+    console.log(turnoJugador)
+    console.log(jugador.negras)
+    console.log( blockPiece.status)
+console.log(data)
+if(socket.nickJugador == turnoJugador && socket.nickJugador == jugador.blancas && data.colorPiece == 'rgb(255, 255, 255)' && blockPiece.status == false){
+  data.turnoJugador = turnoJugador;
+  io.sockets.emit('comunicar seleccion', data);
+}else if(socket.nickJugador == turnoJugador && socket.nickJugador == jugador.negras && data.colorPiece == 'rgb(0, 0, 0)' && blockPiece.status == false)
+data.turnoJugador = turnoJugador;
+io.sockets.emit('comunicar seleccion', data);
   })
   socket.on('validar movimiento peon', function(data) {
     filaDif = data.targetFila - data.fichaFila;
-    if(filaDif == -1 || filaDif == 1){
-
+    columDif = data.targetColumn - data.fichaColumn;
+    data.turnoJugador = turnoJugador;
+    if(filaDif == -1 && (columDif == 1 || columDif == -1) || filaDif == 1 && (columDif == 1 || columDif == -1)){
       if(blockPiece.status == true && data.fichaId==blockPiece.id){
-        console.log(blockPiece.status)
-blockPiece.id=data.targetId;
-        io.sockets.emit('comunicar movimiento', true);
+        data.validate = true;
+        blockPiece.id=data.targetId;
+        io.sockets.emit('comunicar movimiento', data);
       }else if(blockPiece.status == false){
-
-        io.sockets.emit('comunicar movimiento', true);
+        data.validate = true;
+        io.sockets.emit('comunicar movimiento', data);
         blockPiece.id=data.targetId;
         blockPiece.status = true;
-
-
-      }else{
-        console.log(blockPiece.status)
-        console.log(blockPiece.id)
-        console.log(data.fichaId)
       }
-    }
+    }else if(filaDif == -2 || filaDif == 2){
+          if(blockPiece.status == true && data.fichaId==blockPiece.id){
+            if(data.direccion == 'izqArriba' && data.saltoDoble.izqArriba != ''){
+              blockPiece.id=data.targetId;
+              data.validate = true;
+              io.sockets.emit('comunicar movimiento', data);
+            }
+            if(data.direccion == 'izqAbajo' && data.saltoDoble.izqAbajo != ''){
+              blockPiece.id=data.targetId;
+              data.validate = true;
+              io.sockets.emit('comunicar movimiento', data);
+            }
+            if(data.direccion == 'derArriba' && data.saltoDoble.derArriba != ''){
+              blockPiece.id=data.targetId;
+              data.validate = true;
+              io.sockets.emit('comunicar movimiento', data);
+            }
+            if(data.direccion == 'derAbajo' && data.saltoDoble.derAbajo != ''){
+              blockPiece.id=data.targetId;
+              data.validate = true;
+              io.sockets.emit('comunicar movimiento', data);
+            }
+            data.validate = false;
+            io.sockets.emit('comunicar movimiento', data);
+
+          }else if(blockPiece.status == false){
+            if(data.direccion == 'izqArriba' && data.saltoDoble.izqArriba != ''){
+              blockPiece.id=data.targetId;
+              blockPiece.status = true;
+              data.validate = true;
+              io.sockets.emit('comunicar movimiento', data);
+            }
+            if(data.direccion == 'izqAbajo' && data.saltoDoble.izqAbajo != ''){
+              blockPiece.id=data.targetId;
+              blockPiece.status = true;
+              data.validate = true;
+              io.sockets.emit('comunicar movimiento', data);
+            }
+            if(data.direccion == 'derArriba' && data.saltoDoble.derArriba != ''){
+              blockPiece.id=data.targetId;
+              blockPiece.status = true;
+              data.validate = true;
+              io.sockets.emit('comunicar movimiento', data);
+            }
+            if(data.direccion == 'derAbajo' && data.saltoDoble.derAbajo != ''){
+              blockPiece.id=data.targetId;
+              blockPiece.status = true;
+              data.validate = true;
+              io.sockets.emit('comunicar movimiento', data);
+            }
+              data.validate = false;
+              io.sockets.emit('comunicar movimiento', false);
+          }
+        }
   })
 })
 
